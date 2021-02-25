14628a11c9a50305773814377623dd323449d08e
Rhianna So <38052944+rhiannaso@users.noreply.github.com>
2021-02-24 22:31:49 -0800

    Basic build/rebuild functionality

---
+++ Block.py
@@ -0,0 +1,41 @@
+class Block:
+  def __init__(self, op, hp, nonce):
+    self.op = op[:] # List of op, key, val
+    self.hp = hp # String
+    self.nonce = nonce # String
+
+  def getOpType(self):
+    return self.op[0]
+
+  def getOp(self):
+    return self.op
+
+  def setOp(self, op, key, val="none"):
+    if val == "none": # If get op
+      self.op = [op, key]
+    else: # If put op
+      self.op = [op, key, val]
+
+  def getOpString(self):
+    opType = self.getOpType()
+    tmp = "<"
+    tmp += str(self.op[0])
+    tmp += "|"
+    tmp += str(self.op[1])
+    if opType == "put":
+      tmp += "|"
+      tmp += str(self.op[2])
+    tmp += ">"
+    return tmp
+
+  def getHashPtr(self):
+    return self.hp
+
+  def setHashPtr(self, hp):
+    self.hp = hp
+
+  def getNonce(self):
+    return self.nonce
+
+  def setNonce(self, nonce):
+    self.nonce = nonce
