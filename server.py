d49066ccfa29e41ce554dea6cab9eaeb9e3df250
Rhianna So <38052944+rhiannaso@users.noreply.github.com>
2021-03-03 16:16:26 -0800

    Refactored with pickle

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
-import socket, sys, threading, os, queue, time, json, string, random
+import socket, sys, threading, os, queue, time, json, string, random, pickle
 
 from queue import Queue
 from block import Block
+from prepare import Prepare
+from propose import Propose
+from promise import Promise
+from accepted import Accepted
+from decide import Decide
 from hashlib import sha256
 
 #Socket Vars
@@ -13,11 +18,14 @@ SERVERS = []
 CLIENTS = []
 
 #Paxos Vars
-bNum = (0,0,0)
-acceptNum = (0,0,0)
+isLeader = False # Tracks if you're leader or not
+bNum = (0,0,0) # (depth, seqNum, pid)
+acceptNum = (0,0,0) # (depth, seqNum, pid)
 acceptVal = None # aka bottom
-promises = {} # Track num of promises
-accepts = {} # Track number of accepted
+promises = 0 # Track num of promises
+receivedB = (0,0,0) # Track highest b received
+myVal = None # Track value to propose (your block or received block with highest ballotNum)
+accepts = 0 # Track number of accepted
 
 #BlockChain Vars
 portal = {} # Key-value store
@@ -134,52 +142,76 @@ def inputBuild():
 #Paxos Code____________________________________________
 
 # Helper Code_________________________________
-def formatBNum(b):
-    return b[0]+","+b[1]+","+b[2]
+# def formatBNum(b):
+#     return b[0]+","+b[1]+","+b[2]
 
-def formatOpField(val):
-    opType = val[0][0]
-    v = ""
-    if opType == "put":
-        v = f',{val[0][2]}'
-    tmp = f'{opType},{val[0][1]}{v};{val[1]},{val[2]}'
-    return tmp
+# def formatOpField(val):
+#     opType = val[0][0]
+#     v = ""
+#     if opType == "put":
+#         v = f',{val[0][2]}'
+#     tmp = f'{opType},{val[0][1]}{v};{val[1]},{val[2]}'
+#     return tmp
+
+def compareBallots(b1, b2): # Return > 0 if b1 bigger, < 0 if b1 smaller
+    if b1[0] == b2[0]: # If depth is same
+        if b1[1] == b2[1]: # If seqNum is same
+            if b1[2] > b2[2]:
+                return 1
+            else:
+                return -1
+        elif b1[1] > b2[1]:
+            return 1
+        else:
+            return -1
+    elif b1[0] > b2[0]:
+        return 1
+    else:
+        return -1
 
 def formatOp(op):
     opType = op[0]
     tmp = str(op[0])
-    tmp += ","
     tmp += str(op[1])
     if opType == "put":
-        tmp += ","
         tmp += str(op[2])
     return tmp
 
 # Accept Code_________________________________
-def propose(): # Should already be elected 
+def propose(): # Should already be elected
     global tempOp, bNum
     opBlock = tempOp.queue[0] # Access first op in queue
-    op = formatOp(opBlock).replace(",")
+    op = formatOp(opBlock) # Concatenate op together
     nonce = calcNonce(op) # Calculate nonce
-    realOp = tempOp.get() # TODO: Might not want to pop yet
+    #realOp = tempOp.get() # TODO: Might not want to pop yet
     hashVal = calcHashPtr()
-    newBlock = Block(realOp, hashVal, nonce, "tentative") # TODO: Change so not always tentative
-    msg = "propose|"+formatBNum(bNum)+"|"+formatOpField([realOp, hashVal, nonce])
+    newBlock = None
+    if myVal is None: # If all received vals are bottom
+        newBlock = Block(opBlock, hashVal, nonce, "tentative") # TODO: Change so not always tentative
+    else: # If there was a val(s) that was not bottom, use one with highest b
+        newBlock = myVal
+    msg = Propose("propose", bNum, newBlock)
+    pMsg = pickle.dumps(msg)
+    broadcastMsg(pMsg)
+    # msg = "propose|"+formatBNum(bNum)+"|"+formatOpField([realOp, hashVal, nonce])
     # broadcast(msg)
 
 def accept(b, val):
     global bNum
     if b[0] >= bNum[0]:
         acceptNum = b
-        acceptVal = Block(val[0], val[1], val[2], "tentative") # TODO: Change so not always tentative
-        msg = "accepted|"+formatBNum(b)+"|"+formatOpField(val)
+        acceptVal = val # TODO: Change so tag field not always tentative
+        msg = Accepted("accepted", b, val)
+        pMsg = pickle.dumps(msg)
+        # TODO: send pMsg to just the leader
+        # msg = "accepted|"+formatBNum(b)+"|"+formatOpField(val)
         # send_to_leader(msg)
 
 def calcHashPtr():
     global blockchain
     if len(blockchain) > 0: # If not the first block in the blockchain
         prevBlock = blockchain[-1] # Get previous block
-        op = prevBlock.getOpString().replace("<", "").replace(">", "").replace("|", "")
+        op = formatOp(prevBlock.getOp()) # Concatenate op
         nonce = prevBlock.getNonce()
         hp = prevBlock.getHashPtr()
         concat = op+nonce+hp # Concatenate string vals of op, nonce, hash
@@ -197,38 +229,43 @@ def calcNonce(op):
         h = sha256(hashStr.encode()).hexdigest()
         if h[-1].isdigit(): # If the last character is a digit
             num = int(h[-1])
-            if num <= 2 and num >= 0: # And less between 0 and 2
+            if num <= 2 and num >= 0: # And between 0 and 2
                 break
     return nonce
 
 # Decide Code_________________________________
 def decide(b, val):
+    op = val.getOp()
+    hp = val.getHashPtr()
+    nonce = val.getNonce()
     # Append block to blockchain
-    if val[0][0] == "put":
-        addToChain(val[0][0], val[0][1], val[1], val[2], val[0][2])
+    if op[0] == "put":
+        addToChain(op[0], op[1], hp, nonce, op[2])
     else:
-        addToChain(val[0][0], val[0][1], val[1], val[2])
+        addToChain(op[0], op[1], hp, nonce)
     # Update KV
-    cMsg = updateKV(val)
+    cMsg = updateKV(op)
     # Remove op from queue
     tempOp.get()
     # Send ack/value to client
     replyClient(cMsg)
     # Broadcast decision to servers
-    msg = "decide|"+formatBNum(b)+"|"+formatOpField(val)
-    # broadcast(msg)
+    msg = Decide("decide", b, val)
+    pMsg = pickle.dumps(msg)
+    broadcastMsg(pMsg)
+    #msg = "decide|"+formatBNum(b)+"|"+formatOpField(val)
 
-def updateKV(val):
+def updateKV(op):
     global portal
     msg = ""
-    if val[0][0] == "put":
-        key = val[0][1]
-        v = val[0][2]
-        portal[key] = v
+    if op[0] == "put":
+        key = op[1]
+        val = op[2]
+        portal[key] = val
         printKV()
         msg = "ack"
     else:
-        v = portal.get(val[0][1])
+        v = portal.get(op[1])
         if v is None:
             msg = "NO_KEY"
         else:
@@ -240,13 +277,16 @@ def replyClient(msg):
         sock.sendall(f"{msg}".encode("utf8"))
 
 def nonLDecide(b, val):
+    op = val.getOp()
+    hp = val.getHashPtr()
+    nonce = val.getNonce()
     # Append block to blockchain
     if val[0][0] == "put":
-        addToChain(val[0][0], val[0][1], val[1], val[2], val[0][2])
+        addToChain(op[0], op[1], hp, nonce, op[2])
     else:
-        addToChain(val[0][0], val[0][1], val[1], val[2])
+        addToChain(op[0], op[1], hp, nonce)
     # Update KV
-    updateKV(val)
+    updateKV(op)
     printKV()
 
 #Socket Code____________________________________________
@@ -281,20 +321,29 @@ def processInput():
         elif command == "rebuild":
             rebuild()
         elif command == "queue": # TEMP: Used to demo adding to queue
-          tempOp = ["get", "1234567"]
-          addToQueue(tempOp)
-          tempOp = ["put", "7654321", {"phone_number": "111-222-3333"}]
-          addToQueue(tempOp)
+          tmpOp = ["get", "1234567"]
+          addToQueue(tmpOp)
+          tmpOp = ["put", "7654321", {"phone_number": "111-222-3333"}]
+          addToQueue(tmpOp)
           printQueue()
         elif command == "dict": # TEMP: used to demo adding to key-value store
           key = "1234567"
           val = {"phone_number": "111-222-3333"}
           portal[key] = val
           printKV()
+        elif command == "prepare":
+            msg = Prepare("prepare", bNum)
+            pMsg = pickle.dumps(msg)
+            for sock in SERVERS:
+                sock.sendall(pMsg)
 
     return
 
-def broadcast(): # TODO: alter broadcast so message can be passed as param to be broadcast
+def broadcastMsg(msg): # Broadcast pickled messages
+    for sock in SERVERS:
+        sock.sendall(msg)
+
+def broadcast():
     for sock in SERVERS:
         sock.sendall(f"Broadcast Received from Server {processId}".encode("utf8"))
 
@@ -322,58 +371,90 @@ def serverListener():
     MY_SOCK.close()
 
 # Handles received string data and recreates vals
-def parseBVal(data, pType):
-    if pType == "promise":
-        tmp = data.split("|")
-        tmpB = tmp[1].split(",")
-        ballotNum = (tmpB[0], tmpB[1], tmpB[2])
-        tmpB2 = tmp[2].split(",")
-        b = (tmpB2[0], tmpB2[1], tmpB2[2])
-        tmpVal = tmp[3].split(";")
-        op = tmpVal[0].split(",")
-        other = tmpVal[1].split(",")
-        val = [[op[0], op[1], op[2]], other[0], other[1]]
-        return (ballotNum, b, val)
-    else:
-        tmp = data.split("|")
-        tmpB = tmp[1].split(",")
-        b = (tmpB[0], tmpB[1], tmpB[2])
-        tmpVal = tmp[2].split(";")
-        op = tmpVal[0].split(",")
-        other = tmpVal[1].split(",")
-        val = [[op[0], op[1], op[2]], other[0], other[1]]
-        return (b, val)
+# def parseBVal(data, pType):
+#     if pType == "promise":
+#         tmp = data.split("|")
+#         tmpB = tmp[1].split(",")
+#         ballotNum = (tmpB[0], tmpB[1], tmpB[2])
+#         tmpB2 = tmp[2].split(",")
+#         b = (tmpB2[0], tmpB2[1], tmpB2[2])
+#         tmpVal = tmp[3].split(";")
+#         op = tmpVal[0].split(",")
+#         other = tmpVal[1].split(",")
+#         val = [[op[0], op[1], op[2]], other[0], other[1]]
+#         return (ballotNum, b, val)
+#     else:
+#         tmp = data.split("|")
+#         tmpB = tmp[1].split(",")
+#         b = (tmpB[0], tmpB[1], tmpB[2])
+#         tmpVal = tmp[2].split(";")
+#         op = tmpVal[0].split(",")
+#         other = tmpVal[1].split(",")
+#         val = [[op[0], op[1], op[2]], other[0], other[1]]
+#         return (b, val)
 
 #handles responses from the other SERVERS
 def serverResponse(sock, address):
+    global accepts, promises, myVal, receivedB
     while True:
-        data = sock.recv(1024).decode("utf8")
+        #data = sock.recv(1024).decode("utf8")
+        data = sock.recv(1024) # NOTE: if we want to receive any strings now, need to separately decode utf8
         if data:
-            print(data)
-            if "promise" in data: # Assume promise|seqNum,pid,depth|b_seqNum,b_pid,b_depth|op,key,val;hash,nonce
-                parsedVals = parseBVal(data, "promise")
-                ballotNum = parsedVals[0]
-                b = parsedVals[1]
-                val = parsedVals[2]
-                # TODO: Figure out how to store both b and val so that later we can check if all val are bottom and see which b is biggest
-                # If size of promises >= majority: is now leader and propose()
-            if "propose" in data: # Assume propose|seqNum,pid,depth|op,key,val;hash,nonce
-                parsedVals = parseBVal(data, "propose")
-                b = parsedVals[0]
-                val = parsedVals[1]
-                # Clear promises?
-            if "accepted" in data: # Assume accepted|seqNum,pid,depth|op,key,val;hash,nonce
-                parsedVals = parseBVal(data, "accepted")
-                b = parsedVals[0]
-                val = parsedVals[1]
-                # Add to accepts
-                # If size of accepts >= majority: decide(b, val)
-            if "decide" in data: # Assume decide|seqNum,pid,depth|op,key,val;hash,nonce
-                parsedVals = parseBVal(data, "decide")
-                b = parsedVals[0]
-                val = parsedVals[1]
-                nonLDecide(b, val)
-                # Clear accepts?
+            #print(data)
+            dataMsg = pickle.loads(data)
+            if isinstance(dataMsg, Prepare): # Receiving PREPARE
+                bal = dataMsg.getBNum()
+            if isinstance(dataMsg, Promise): # Receiving PROMISE
+                ballotNum = dataMsg.getBNum()
+                b = dataMsg.getB()
+                val = dataMsg.getBlock() # use val.getOp(), val.getHashPtr(), val.getNonce() to get fields
+                # If val is None, do nothing (myVal is still your block)
+                if val is not None: # If val is not None and b > receivedB, set myVal = val
+                    if compareBallots(b, receivedB) > 0:
+                        myVal = val
+                # Increment promises
+                promises += 1
+                if promises >= 3: 
+                    propose() # Is now leader
+            if isinstance(dataMsg, Propose): # Receiving PROPOSE (aka ACCEPT)
+                b = dataMsg.getBNum()
+                val = dataMsg.getBlock() # use val.getOp(), val.getHashPtr(), val.getNonce() to get fields
+                accept(b, val)
+            if isinstance(dataMsg, Accepted): # Receiving ACCEPTED
+                b = dataMsg.getBNum()
+                val = dataMsg.getBlock() # use val.getOp(), val.getHashPtr(), val.getNonce() to get fields
+                accepts += 1
+                if accepts >= 3: 
+                    decide(b, val)
+            if isinstance(dataMsg, Decide): # Receiving DECIDE
+                b = dataMsg.getBNum()
+                val = dataMsg.getBlock() # use val.getOp(), val.getHashPtr(), val.getNonce() to get fields
+                nonLDecide(b, val) # decide as a participant
+
+            # if "promise" in data: # Assume promise|depth,seqNum,pid|b_depth,b_seqNum,b_pid|op,key,val;hash,nonce
+            #     parsedVals = parseBVal(data, "promise")
+            #     ballotNum = parsedVals[0]
+            #     b = parsedVals[1]
+            #     val = parsedVals[2]
+            #     # TODO: Figure out how to store both b and val so that later we can check if all val are bottom and see which b is biggest
+            #     # If size of promises >= majority: is now leader and propose()
+            # if "propose" in data: # Assume propose|seqNum,pid,depth|op,key,val;hash,nonce
+            #     parsedVals = parseBVal(data, "propose")
+            #     b = parsedVals[0]
+            #     val = parsedVals[1]
+            #     # Clear promises?
+            # if "accepted" in data: # Assume accepted|seqNum,pid,depth|op,key,val;hash,nonce
+            #     parsedVals = parseBVal(data, "accepted")
+            #     b = parsedVals[0]
+            #     val = parsedVals[1]
+            #     # Add to accepts
+            #     # If size of accepts >= majority: decide(b, val)
+            # if "decide" in data: # Assume decide|seqNum,pid,depth|op,key,val;hash,nonce
+            #     parsedVals = parseBVal(data, "decide")
+            #     b = parsedVals[0]
+            #     val = parsedVals[1]
+            #     nonLDecide(b, val)
+            #     # Clear accepts?
         if not data:
             sock.close()
             break
